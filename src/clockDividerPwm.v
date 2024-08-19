50d0be1a959793ac56de37bdd43da5854fbb7918
thexeno <thexeno@users.noreply.github.com>
2024-08-19 23:05:32 +0200

    Update clockDividerPwm.v

--- src/clockDividerPwm.v
+++ src/clockDividerPwm.v
@@ -44,7 +44,7 @@ reg     clkPrescSig = 1'b0;
 //    end
 assign   clkPresc = clkPrescSig;   
 
-always @(posedge clk)
+always @(negedge clk)
    begin : mainprocess
    if (reset == 1'b 0)
       begin
