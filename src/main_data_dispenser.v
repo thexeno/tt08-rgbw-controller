dbe7cbdcfdc7ce7597aab6eb9517d6f90abcc38f
thexeno <thexeno@users.noreply.github.com>
2024-08-21 12:12:54 +0200

    Update main_data_dispenser.v

--- src/main_data_dispenser.v
+++ src/main_data_dispenser.v
@@ -13,14 +13,22 @@ module rgbw_data_dispencer (
     input wire rdy,
     input wire clk,
     input wire clk_half,
-    output reg [7:0] lint_spi_out,
-    output reg [7:0] red_spi_out,
-    output reg [7:0] green_spi_out,
-    output reg [7:0] blue_spi_out,
-    output reg [7:0] white_spi_out,
-    output reg [7:0] colorIdx_spi_out,
-    output reg [7:0] mode_spi_out
+    output wire [7:0] lint_spi_out,
+    output wire [7:0] red_spi_out,
+    output wire [7:0] green_spi_out,
+    output wire [7:0] blue_spi_out,
+    output wire [7:0] white_spi_out,
+    output wire [7:0] colorIdx_spi_out,
+    output wire [7:0] mode_spi_out
 );
+
+    reg [7:0] lint_spi_out_reg = 0;
+    reg [7:0] red_spi_out_reg = 0;
+    reg [7:0] green_spi_out_reg = 0;
+    reg [7:0] blue_spi_out_reg = 0;
+    reg [7:0] white_spi_out_reg = 0;
+    reg [7:0] colorIdx_spi_out_reg = 0;
+    reg [7:0] mode_spi_out_reg = 0;
 
     reg [7:0] lint_spi = 8'b00000000;
     reg [7:0] red_spi = 8'b00000000;
@@ -34,12 +42,26 @@ module rgbw_data_dispencer (
     reg rdy_prev = 1'b0;
     //reg sync_char = 1'b0;
 
+    assign lint_spi_out = lint_spi_out_reg;
+    assign red_spi_out = red_spi_out_reg;
+    assign green_spi_out = green_spi_out_reg;
+    assign blue_spi_out  = blue_spi_out_reg;
+    assign white_spi_out = white_spi_out_reg;
+    assign colorIdx_spi_out = colorIdx_spi_out_reg;
+    assign mode_spi_out  = mode_spi_out_reg;
 
 always @(posedge clk) 
 begin
    if (clk_half == 1'b0)
    begin
     if (reset == 1'b0)  begin
+                lint_spi_out_reg <= 8'b00000000;
+                red_spi_out_reg <= 8'b00000000;
+                green_spi_out_reg <= 8'b00000000;
+                blue_spi_out_reg <= 8'b00000000;
+                white_spi_out_reg <= 8'b00000000;
+                colorIdx_spi_out_reg <= 8'b00000000;
+                mode_spi_out_reg <= 8'b00000000;
                 lint_spi <= 8'b00000000;
                 colorIdx_spi <= 8'b00000000;
                 white_spi <= 8'b00000000;
@@ -59,8 +81,7 @@ begin
                 // mode_sync <= 8'h00;
                 lint_spi_out <= lint_spi;
                 colorIdx_spi_out <= 8'b00000000;
-                mode_spi_out <= 8'b000000000;
-                red_spi_out <= 8'b000000000;
+                red_spi_out <= 8'b00000000;
                 green_spi_out <= 8'b00000000;
                 blue_spi_out <= 8'b00000000;
                 white_spi_out <= 8'b00000000;
@@ -104,14 +125,14 @@ begin
                         //byte_cnt_spi <= byte_cnt_spi + 1;
                     end
                     4'h7: begin
-                        mode_spi_out <= buffRx_spi; // rimani in questo stato sempre fino a nuovo RDY
+                        mode_spi_out_reg <= buffRx_spi; // rimani in questo stato sempre fino a nuovo RDY
                         byte_cnt_spi <= 4'h0;
-                        lint_spi_out <= lint_spi;
-                        colorIdx_spi_out <= colorIdx_spi;
-                        red_spi_out <= red_spi;     //are 16bit for optimizing the reuslt of mult in color_Gen, works better with the synthesizer
-                        green_spi_out <= green_spi;
-                        blue_spi_out <= blue_spi;
-                        white_spi_out <= white_spi;
+                        lint_spi_out_reg <= lint_spi;
+                        colorIdx_spi_out_reg <= colorIdx_spi;
+                        red_spi_out_reg <= red_spi;     //are 16bit for optimizing the reuslt of mult in color_Gen, works better with the synthesizer
+                        green_spi_out_reg <= green_spi;
+                        blue_spi_out_reg <= blue_spi;
+                        white_spi_out_reg <= white_spi;
                     end
                     default: byte_cnt_spi <= 4'h0;
                 endcase
