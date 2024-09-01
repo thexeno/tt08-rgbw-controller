e29b89fd61f222b7c37b1f953f871d3dcd733938
thexeno <thexeno@users.noreply.github.com>
2024-09-01 17:57:55 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -19,7 +19,7 @@ PREAMB_POS = 0
 
 PIN_CS = 4
 PIN_SCK = 5
-PIN_MOSI = 6
+PIN_MOSI = 3
 
 async def clear_input(dut, b: int):
         global ui_in
@@ -33,14 +33,14 @@ async def set_input(dut, b: int):
     dut.ui_in.value = ui_in
     await ClockCycles(dut.clk, 1)
 
-async def test_colorwheel(dut):
+async def test_colorwheel(dut, idx):
     dataPayload = [0x55, 0xff, 0x24, 0x00, 0xff, 0x00, 0xaa, 0xa4]
     dataPayload[R_POS] = 0x00
     dataPayload[G_POS] = 0x00
     dataPayload[B_POS] = 0x00
     dataPayload[W_POS] = 0x39
     dataPayload[INT_POS] = 0xFF
-    dataPayload[IDX_POS] = 0xB8
+    dataPayload[IDX_POS] = idx
     dataPayload[MODE_POS] = 0xa4
     dataPayload[PREAMB_POS] = 0x55
 
@@ -75,19 +75,19 @@ async def SPI_send(dut, DATA: int):
     await ClockCycles(dut.clk, 5)
     # Send SPI data
     for i in range(8):
-        dut._log.info(f"SPI send: {i}")
+        #dut._log.info(f"SPI send: {i}")
         if (DATA << i) & 0x80 == 0x80: # Check if highest bit is set      
         # Clear bit 7 in the current value and then set it to the desired value
         # current_value = dut.uo_out.value.integer
             dut.ui_in.value = (dut.ui_in.value) | (0x1 << PIN_MOSI)
             await ClockCycles(dut.clk, 1)
-            dut._log.info(f"DATA: 1")
-            print ((dut.ui_in))
+            #dut._log.info(f"DATA: 1")
+            #print ((dut.ui_in))
         else:
             dut.ui_in.value = dut.ui_in.value & ~(0x1 << PIN_MOSI)
             await ClockCycles(dut.clk, 1)
-            dut._log.info(f"DATA: 0")
-            print (dut.ui_in)
+            #dut._log.info(f"DATA: 0")
+            #print (dut.ui_in)
 
          
         dut.ui_in.value = (dut.ui_in.value) | (0x1 << PIN_SCK)
@@ -104,7 +104,7 @@ async def user_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
 
@@ -114,11 +114,11 @@ async def user_project(dut):
     dut.rst_n.value = 0
     dut.ui_in.value = 0 # reset 
     await ClockCycles(dut.clk, 10)
-    dut.ui_in.value = dut.ui_in.value | (0x1 << 3) # set the test pin
+    dut.ui_in.value = dut.ui_in.value | (0x1 << 6) # set the test pin
     await ClockCycles(dut.clk, 1000)
     #assert (dut.uo_out.value[7]) == (0)
     dut.ena.value = 1
-    dut.ui_in.value = dut.ui_in.value | (0x1 << 3) # set the test pin
+    dut.ui_in.value = dut.ui_in.value | (0x1 << 6) # set the test pin
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 130) # more than the reset delay count of the clk_prescaler
     dut.ui_in.value = dut.ui_in.value & ~(0x1 << 7) # reset the clock gen
@@ -129,7 +129,7 @@ async def user_project(dut):
     dut.ui_in.value = (dut.ui_in.value) | (0x1 << PIN_CS)
     await ClockCycles(dut.clk, 10)
 
-    await test_colorwheel(dut)
+    await test_colorwheel(dut, 0xb8)
     await ClockCycles(dut.clk, 10000)
 
     await test_rgb(dut)
@@ -139,7 +139,7 @@ async def user_project(dut):
     await ClockCycles(dut.clk, 10)
     await SPI_send(dut, 0xAA)
     await ClockCycles(dut.clk, 10)
-    dut.ui_in.value = dut.ui_in.value & ~(0x1 << 3) # reset the test pin
+    dut.ui_in.value = dut.ui_in.value & ~(0x1 << 6) # reset the test pin
     await ClockCycles(dut.clk, 1000)
 
 
@@ -154,8 +154,13 @@ async def user_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 130)
-    await test_colorwheel(dut)
-    await ClockCycles(dut.clk, 10000)
+    await test_colorwheel(dut, 0xb8)
+    await ClockCycles(dut.clk, 4000)
+
+    for i in range(255):
+        await test_colorwheel(dut, i)
+        print("\n", i)
+        await ClockCycles(dut.clk, 4000)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
