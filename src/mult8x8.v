8b4fdf5d792a35c15045c08105ef9bbfc6e98a8c
thexeno <thexeno@users.noreply.github.com>
2025-04-14 00:29:29 +0200

    Delta resource occupation, Multipl Unit
    To be compared against the 4-tuple with the Multiplier Unit fully implemented. Just the * vs + is made. The unit, with the ready signals, must have been inserted to avoid logic optimizations

--- src/mult8x8.v
+++ src/mult8x8.v
@@ -28,12 +28,49 @@ module mult8x8_module (
  reg [3:0] seq = 4'h0;
  reg ld_latch = 1'b0;
  reg ld_prev = 1'b0;
- 
+
 
 always @(posedge clk)
    begin
-      mult_rdy <= 1'b0;
-      result <= a + b;
+   if (reset == 1'b0)
+      begin
+         result <= 16'h0000;
+         mult_rdy <=  1'b 0; 
+         seq <= 4'b0000;
+         ld_latch <= 1'b0;  
+      end
+   else 
+   begin
+      ld_latch <= ld;
+      ld_prev <= ld_latch;
+      if (seq == 4'h0) begin
+         if (ld == 1'b0)
+         begin
+            mult_rdy <= 1'b0;
+         end
+            
+         if (ld_prev == 1'b0 && ld_latch == 1'b1) // at the rising edge of load, store the operands
+         begin
+            a_sig <= a;
+            b_sig <= b;
+            mult_rdy <= 1'b0;
+            seq <= 4'h1;
+         end
+      end
+      else if (seq == 4'h1) // execute the multiplication and assert to the rdy signal
+      begin
+         result <= a_sig + b_sig;
+         mult_rdy <= 1'b1;
+         seq <= 4'h2;
+      end
+      else if (seq == 4'h2) // to synchronize with the color wheel, extends the rdy by one tclk. probably useless,
+                            // can be avoided, but it works. it took some time to find right sync, so is fine for now
+      begin
+         seq <= 4'h0;
+      end
+      else seq <= 4'h0;
+
+      end
    end
 endmodule 
 
