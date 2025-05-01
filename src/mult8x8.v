427c5f87b2ff4acc2ca1da986a46f3f3a9ed8759
thexeno <thexeno@users.noreply.github.com>
2025-05-01 08:35:59 +0200

    Revert "Delta resource occupation, Multipl Unit (further optimized)"
    This reverts commit 68714aad251d046df0a38bed8cb28e243c079681.

--- src/mult8x8.v
+++ src/mult8x8.v
@@ -25,6 +25,10 @@ module mult8x8_module (
 
  reg[7:0] a_sig = 8'h00;
  reg[7:0] b_sig = 8'h00;
+ reg [3:0] seq = 4'h0;
+ reg ld_latch = 1'b0;
+ reg ld_prev = 1'b0;
+
 
 always @(posedge clk)
    begin
@@ -32,16 +36,41 @@ always @(posedge clk)
       begin
          result <= 16'h0000;
          mult_rdy <=  1'b 0; 
+         seq <= 4'b0000;
          ld_latch <= 1'b0;  
       end
    else 
    begin
-         if (ld == 1'b1) // at the rising edge of load, store the operands
+      ld_latch <= ld;
+      ld_prev <= ld_latch;
+      if (seq == 4'h0) begin
+         if (ld == 1'b0)
          begin
+            mult_rdy <= 1'b0;
+         end
+            
+         if (ld_prev == 1'b0 && ld_latch == 1'b1) // at the rising edge of load, store the operands
+         begin
+            a_sig <= a;
+            b_sig <= b;
+            mult_rdy <= 1'b0;
+            seq <= 4'h1;
+         end
+      end
+      else if (seq == 4'h1) // execute the multiplication and assert to the rdy signal
+      begin
          result <= a_sig + b_sig;
          mult_rdy <= 1'b1;
-         end
+         seq <= 4'h2;
+      end
+      else if (seq == 4'h2) // to synchronize with the color wheel, extends the rdy by one tclk. probably useless,
+                            // can be avoided, but it works. it took some time to find right sync, so is fine for now
+      begin
+         seq <= 4'h0;
+      end
+      else seq <= 4'h0;
+
+      end
    end
-end
 endmodule 
 
